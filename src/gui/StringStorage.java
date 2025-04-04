af2b5012a9669948e63079b3241bbb0c60cf8e96
Jonathan Hollinger <Hollinjf@dukes.jmu.edu>
2025-04-04 15:33:09 -0400

    Commit for self, added StringStorage class to hold stuff

---
+++ src/gui/StringStorage.java
@@ -0,0 +1,58 @@
+package gui;
+
+import java.util.ArrayDeque;
+
+/**
+ * Stores string to be taken from other points in the program.
+ */
+public class StringStorage 
+{
+  private static ArrayDeque<String> arr;
+  
+  /**
+   * adds to storage.
+   * @param s input
+   */
+  public static void add(final String s) 
+  {
+    arr.add(s);
+  }
+  
+  /**
+   * removes from storage.
+   */
+  public static void remove() 
+  {
+    arr.pollLast();
+  }
+  
+  /**
+   * clears storage.
+   */
+  public static void clear() 
+  {
+    arr = new ArrayDeque<String>();
+  }
+  
+  /**
+   * Returns universal string storage.
+   * @return arr
+   */
+  public static ArrayDeque<String> getArr() 
+  {
+    return new ArrayDeque<String>(arr);
+  }
+  
+  /**
+   * Testing method to dump the whole array.
+   */
+  public void dumpString() 
+  {
+
+    for (String s: arr) 
+    {
+      System.out.print(s);
+    }
+    System.out.println();
+  }
+}
--- src/testing/TestTokenParse.java
+++ src/testing/TestTokenParse.java
@@ -13,9 +13,9 @@ class TestTokenParse
     String equation = "5 + 4";
     //assertEquals(9, calculate(equation));
     String eqTwo = "3 * (4 + 1)";
-    //assertEquals(15, calculate(equation));
+    //assertEquals(15, calculate(eqTwo));
     String eqThree = "5 / 2";
-    //assertEquals(2.5, calculate(equation));
+    //assertEquals(2.5, calculate(eqThree));
     fail("Not yet implemented");
   }
 
