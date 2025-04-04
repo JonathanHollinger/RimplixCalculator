26ce9dc6ab0b3b5f2e1e4775f18720f5289f9003
Jonathan Hollinger <Hollinjf@dukes.jmu.edu>
2025-04-04 15:07:06 -0400

    Added basic test file

---
+++ src/testing/TestTokenParse.java
@@ -0,0 +1,22 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestTokenParse
+{
+
+  @Test
+  void testCalculate()
+  {
+    String equation = "5 + 4";
+    //assertEquals(9, calculate(equation));
+    String eqTwo = "3 * (4 + 1)";
+    //assertEquals(15, calculate(equation));
+    String eqThree = "5 / 2";
+    //assertEquals(2.5, calculate(equation));
+    fail("Not yet implemented");
+  }
+
+}
