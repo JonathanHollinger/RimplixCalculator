2d6e309a7879d9eb8a1a5cacaeffbc87231e22cc
Nicky5x <franknv@dukes.jmu.edu>
2025-04-06 19:55:38 -0400

    File Menu bar

--- src/gui/PINPadWindow.java
+++ src/gui/PINPadWindow.java
@@ -8,41 +8,53 @@ import javax.swing.*;
  */
 public class PINPadWindow extends JFrame
 {
-  
-    /**
-   * 
-   */
+
+  /**
+  * 
+  */
   private static final long serialVersionUID = 1L;
+  private static final String EXIT_ITEM = "Exit";
+  private static final String FILE_MENU = "File";
+  private static final String HELP_MENU = "Help";
 
-    /**
-     * Default Constructor.
-     */
-    public PINPadWindow()
-    {
-        super();
-        setupLayout();
-        pack();
-        setResizable(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    
-    private void setupLayout()
-    {
-      // ImageIcon img = new ImageIcon("media/iconRmplex.png");
-      // setIconImage(img.getImage());
-      
-      setSize(600, 500); 
-      setTitle("RimpleX");
-      
+  /**
+   * Default Constructor.
+   */
+  public PINPadWindow()
+  {
+    super();
+    setupLayout();
+    pack();
+    setResizable(true);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
 
-      Container contentPane = getContentPane();
-      contentPane.setLayout(new BorderLayout());
+  private void setupLayout()
+  {
+    // ImageIcon img = new ImageIcon("media/iconRmplex.png");
+    // setIconImage(img.getImage());
+    JMenuBar menuBar = new JMenuBar();
+    setJMenuBar(menuBar);
 
-      Display display = new Display();        
-      contentPane.add(display, BorderLayout.NORTH);
-      
-      NumberPad numberPad = new NumberPad(display);
-      contentPane.add(numberPad, BorderLayout.CENTER);
-    }
+    JMenu fileMenu = new JMenu(FILE_MENU);
+    JMenuItem exitItem = new JMenuItem(EXIT_ITEM);
+    JMenuItem helpItem = new JMenuItem(HELP_MENU);
+    exitItem.addActionListener(e -> System.exit(0));
+    fileMenu.add(exitItem);
+    fileMenu.add(helpItem);
+    menuBar.add(fileMenu);
+
+    setSize(600, 500);
+    setTitle("RimpleX");
+
+    Container contentPane = getContentPane();
+    contentPane.setLayout(new BorderLayout());
+
+    Display display = new Display();
+    contentPane.add(display, BorderLayout.NORTH);
+
+    NumberPad numberPad = new NumberPad(display);
+    contentPane.add(numberPad, BorderLayout.CENTER);
+  }
 
 }
