a97443e7d6ffc28d92662ae6c266311ab273fa44
Logan A. <Altomolx@dukes.jmu.edu>
2025-04-07 14:01:43 -0400

    Added additional items to menu bar to comply with design documents.

--- src/gui/PINPadWindow.java
+++ src/gui/PINPadWindow.java
@@ -16,6 +16,9 @@ public class PINPadWindow extends JFrame
   private static final String EXIT_ITEM = "Exit";
   private static final String FILE_MENU = "File";
   private static final String HELP_MENU = "Help";
+  private static final String VIEW_MENU = "View"; 
+  private static final String ABOUT_MENU = "About"; 
+  
 
   /**
    * Default Constructor.
@@ -37,12 +40,18 @@ public class PINPadWindow extends JFrame
     setJMenuBar(menuBar);
 
     JMenu fileMenu = new JMenu(FILE_MENU);
+    JMenu viewMenu = new JMenu(VIEW_MENU);
+    JMenu helpMenu = new JMenu(HELP_MENU);
     JMenuItem exitItem = new JMenuItem(EXIT_ITEM);
+    JMenuItem aboutItem = new JMenuItem(ABOUT_MENU);
     JMenuItem helpItem = new JMenuItem(HELP_MENU);
     exitItem.addActionListener(e -> System.exit(0));
     fileMenu.add(exitItem);
-    fileMenu.add(helpItem);
+    helpMenu.add(aboutItem);
+    helpMenu.add(helpItem);
     menuBar.add(fileMenu);
+    menuBar.add(viewMenu);
+    menuBar.add(helpMenu);
 
     setSize(600, 500);
     setTitle("RimpleX");
